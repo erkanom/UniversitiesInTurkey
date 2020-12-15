34c2af03907ad0f272e7686eb8799e3ece0b6a79
ÖMER ERKAN <70490959+erkanom@users.noreply.github.com>
2020-12-15 03:05:31 +0300

    Getting departments
    I adding all departments to uni struct

--- Main.go
+++ Main.go
@@ -11,9 +11,15 @@ import (
 )
 
 type uni struct {
-	name string
-	id   int
+	name  string
+	id    int
+	depts []int
 }
+
+// type dept struct {
+// 	id   int
+// 	name string
+// }
 
 func main() {
 	url := "https://yokatlas.yok.gov.tr/lisans-anasayfa.php"
@@ -54,5 +60,44 @@ func main() {
 			mapOfUni[index] = *tempUni
 		}
 	}
+	for _, value := range mapOfUni {
+		getDep(&value)
+	}
+}
+func getDep(uni *uni) {
 
+	newUrl := "https://yokatlas.yok.gov.tr/lisans-univ.php?u="
+
+	urlId := strconv.Itoa(uni.id)
+
+	newUrl += urlId
+	resp, err := http.Get(newUrl)
+	if err != nil {
+		log.Fatal("Page not found")
+		panic(err)
+
+	}
+	defer resp.Body.Close()
+	content, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+
+	}
+	strContent := string(content[:])
+	scanner := bufio.NewScanner(strings.NewReader(strContent))
+	//index := 0
+	for scanner.Scan() {
+		var temp string
+		temp = scanner.Text()
+		if strings.Contains(temp, "lisans.php?y=") {
+			temp = strings.ReplaceAll(temp, "<a data-parent=\"#\" href=\"lisans.php?y=", " ")
+			temp = strings.ReplaceAll(temp, "\">", "")
+			temp = strings.Trim(temp, " ")
+			tempInt, err := strconv.Atoi(temp)
+			if err != nil {
+				fmt.Print("convert error string to int ")
+			}
+			uni.depts = append(uni.depts, tempInt)
+		}
+	}
 }
